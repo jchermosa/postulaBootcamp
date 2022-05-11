f0b4abc3f318ea73ec18d53c86b11dcca420f8b2
Nahuel <nahuelmereles1@gmail.com>
2022-05-11 18:16:56 -0400

    Creacion de entidades profesor,usuario,lenguaje,bootcamp con respectivos saveServlets y DAO

---
+++ src/main/java/com/roshka/proyectofinal/entity/Usuario.java
@@ -0,0 +1,53 @@
+package com.roshka.proyectofinal.entity;
+
+public class Usuario {
+    private int id;
+    private String nombre,apellido,correo,contrasena;
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String nombre, String apellido, String correo, String contrasena) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contrasena = contrasena;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
