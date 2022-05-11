c7b5913b98829ce9856cdf02ee0c94cdd45e0e10
JoseBaezx <rajose.baez@gmail.com>
2022-05-11 15:08:25 -0400

    Se agrega clase data base principal

---
+++ src/main/java/com/roshka/proyectofinal/DataBase.java
@@ -0,0 +1,28 @@
+package com.roshka.proyectofinal;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataBase {
+
+    public static Connection getConnection(){
+        Connection con=null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            con= DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/bootcamp_market",
+                            "postgres", "postgres");
+
+            if(con != null){
+                System.out.println("---> CONNECTED TO SERVER");
+            }else {
+                System.out.println("---> UNABLE TO CONNECTED TO SERVER");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        return con;
+    }
+}
