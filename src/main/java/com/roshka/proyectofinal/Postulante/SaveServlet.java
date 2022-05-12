27b3f24e31110365548d1a1530514e17a3648096
Yovan Martinez <martinezyovam@gmail.com>
2022-05-12 12:49:51 -0400

    Trabajando validacion de correo

---
+++ src/main/java/com/roshka/proyectofinal/Postulante/SaveServlet.java
@@ -0,0 +1,94 @@
+package com.roshka.proyectofinal.Postulante;
+
+import com.roshka.proyectofinal.DataBase;
+import com.roshka.proyectofinal.entity.Postulante;
+import com.roshka.proyectofinal.entity.Bootcamp;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+@WebServlet("/SaveServlet")
+public class SaveServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out=response.getWriter();
+        boolean correoRepetido = false;
+
+        try {
+            Connection con = DataBase.getConnection();
+            //
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT correo FROM postulante;");
+            //
+
+
+            String nombre=request.getParameter("nombre");
+            String apellido=request.getParameter("apellido");
+            int cedula=Integer.parseInt(request.getParameter("cedula"));
+            String correo=request.getParameter("correo");
+            while (rs.next()){
+                String correoBase =rs.getString("correo");
+                if(correo.equals(correo)){
+                    correoRepetido = true;
+                }
+            }
+            String telefono=request.getParameter("telefono");
+            String direccion=request.getParameter("direccion");
+            boolean experienciaProgramando = false;
+            boolean experienciaLaboral = false;
+            boolean universidad = false;
+            boolean notebook = false;
+            if (request.getParameter("experiencia_laboral") != null){
+                experienciaLaboral = true;
+            }
+            if (request.getParameter("experiencia_programando") != null) {
+                experienciaProgramando = true;
+            }
+            if (request.getParameter("notebook") != null){
+                notebook = true;
+            }
+            if (request.getParameter("universidad") != null){
+                universidad = true;
+            }
+
+            if (!correoRepetido){
+                Bootcamp bootcamp = new Bootcamp();
+                Postulante postulante=new Postulante();
+                postulante.setNombre(nombre);
+                postulante.setApellido(apellido);
+                postulante.setNro_cedula(cedula);
+                postulante.setCorreo(correo);
+                postulante.setTelefono(telefono);
+                postulante.setDireccion(direccion);
+                postulante.setExpLaboral(experienciaLaboral);
+                postulante.setEstudioUniversitario(universidad);
+                postulante.setNotebook(notebook);
+                postulante.setBootcampId(1);
+                postulante.setAceptado(false);
+                int status=PostulanteDao.save(postulante);
+
+                if(status>0){
+                    out.print("<p>Record saved successfully!</p>");
+                    request.getRequestDispatcher("index.html").include(request, response);
+                }else{
+                    out.println("Sorry! unable to save record");
+                }
+            }
+
+        }catch (Exception ex){ex.printStackTrace();}
+
+
+        out.close();
+    }
+}
