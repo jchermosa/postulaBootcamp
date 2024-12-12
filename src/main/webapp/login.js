9060d67dc82526d789237a10065d7e75a4f0a466
jgarcete <jcgarcete2@gmail.com>
2024-12-12 16:49:01 -0300

    Se crea el template y animaciones para el login, pendiente a modificaciones aun

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -1,61 +1,52 @@
-var inP     =   $('.input-field');
+$(document).ready(function () {
+    var inP = $('.input-field');
 
-inP.on('blur', function () {
-    if (!this.value) {
-        $(this).parent('.f_row').removeClass('focus');
-    } else {
-        $(this).parent('.f_row').addClass('focus');
-    }
-}).on('focus', function () {
-    $(this).parent('.f_row').addClass('focus');
-    $('.btn').removeClass('active');
-    $('.f_row').removeClass('shake');
-});
-
-
-$('.resetTag').click(function(e){
-    e.preventDefault();
-    $('.formBox').addClass('level-forget').removeClass('level-reg');
-});
-
-$('.back').click(function(e){
-    e.preventDefault();
-    $('.formBox').removeClass('level-forget').addClass('level-login');
-});
-
-
-
-$('.regTag').click(function(e){
-    e.preventDefault();
-    $('.formBox').removeClass('level-reg-revers');
-    $('.formBox').toggleClass('level-login').toggleClass('level-reg');
-    if(!$('.formBox').hasClass('level-reg')) {
-        $('.formBox').addClass('level-reg-revers');
-    }
-});
-$('.btn').each(function() {
-     $(this).on('click', function(e){
-        e.preventDefault();
-        
-        var finp =  $(this).parent('form').find('input');
-       
-       console.log(finp.html());
-       
-        if (!finp.val() == 0) {
-            $(this).addClass('active');
+    // Focus/Blur logic
+    inP.on('blur', function () {
+        if (!this.value) {
+            $(this).parent('.f_row').removeClass('focus');
+        } else {
+            $(this).parent('.f_row').addClass('focus');
         }
-        
+    }).on('focus', function () {
+        $(this).parent('.f_row').addClass('focus');
+        $('.btn').removeClass('active');
+        $('.f_row').removeClass('shake');
+    });
+
+    // Forgot Password button
+    $('.resetTag').click(function (e) {
+        e.preventDefault();
+        $('.formBox').addClass('level-forget').removeClass('level-reg');
+    });
+
+    // Back button
+    $('.back').click(function (e) {
+        e.preventDefault();
+        $('.formBox').removeClass('level-forget').addClass('level-login');
+    });
+
+    // Register button
+    $('.regTag').click(function (e) {
+        e.preventDefault();
+        $('.formBox').toggleClass('level-login level-reg');
+    });
+
+    // Submit button
+    $('.btn').on('click', function (e) {
+        e.preventDefault();
+        var finp = $(this).closest('form').find('.input-field');
+
+        if (finp.val().trim() !== '') {
+            $(this).addClass('active');
+        } else {
+            finp.parent('.f_row').addClass('shake');
+        }
+
         setTimeout(function () {
-            
-            inP.val('');
-            
+            finp.val('');
             $('.f_row').removeClass('shake focus');
             $('.btn').removeClass('active');
-            
         }, 2000);
-         
-        if(inP.val() == 0) {
-            inP.parent('.f_row').addClass('shake');
-        }
     });
 });
